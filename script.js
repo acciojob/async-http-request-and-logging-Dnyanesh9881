74d5029bccb91292e07c282bab61a87673da618f
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-03-21 20:01:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+async function fun(){
+	try {
+	const res=await fetch("https://jsonplaceholder.typicode.com/todos/1")
+		const data=await res.json();
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
+}
+fun();
